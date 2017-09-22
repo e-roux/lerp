7d4f760ac6752c600e23bdc4bf022035e766036d
gwin-zegal <gwin-zegal@users.noreply.github.com>
2017-09-22 07:09:20 +0200

    Fix PEP8

--- lerp/util.py
+++ lerp/util.py
@@ -12,7 +12,8 @@ from openpyxl import load_workbook
 from openpyxl.utils import rows_from_range
 import logging
 logging.basicConfig(level=logging.WARNING)
-logger = logging.getLogger() # __name__
+logger = logging.getLogger()  # __name__
+
 
 def to_clipboard_for_excel(array):
     r"""Copy an array into a string format acceptable by Excel.
@@ -21,20 +22,21 @@ def to_clipboard_for_excel(array):
     """
     try:
         import win32clipboard as clipboard
-    except:
+    except ModuleNotFoundError:
         raise ModuleNotFoundError("Module not found. Not on win32 platform.")
     else:
         # Create string from array
         array_string = ""
 
         if len(np.shape(array)) == 1:
-            array_string += "\t".join([str(_e).replace(".",",") for _e in array])
+            array_string += "\t".join([str(_e).replace(".", ",")
+                                      for _e in array])
 
         else:
             for line in array:
-                array_string += "\t".join([str(_e).replace(".",",") for _e in line])
+                array_string += "\t".join([str(_e).replace(".", ",")
+                                          for _e in line])
                 array_string += "\r\n"
-
 
         # Put string into clipboard (open, clear, set, close)
         clipboard.OpenClipboard()
@@ -42,9 +44,9 @@ def to_clipboard_for_excel(array):
         clipboard.SetClipboardText(array_string)
         clipboard.CloseClipboard()
 
+
 def col2int(n):
-    """
-    Conversion utility column string to Integer.
+    """Conversion utility column string to Integer.
 
     Returns
     ----------
@@ -52,13 +54,14 @@ def col2int(n):
 
     Note
     ----------
-    See from get_column_letter() in openpyxl.utils.cell
+    See get_column_letter() in openpyxl.utils.cell
     """
     from math import pow
-    res=0
+    res = 0
     for i, v in enumerate(n.upper()[::-1]):
-        res += ( ord(v) - 64 ) * pow(26,i)
+        res += (ord(v) - 64) * pow(26, i)
     return int(res-1)
+
 
 class xlsx(object):
     """Generic class for retrieving data from xlsx file.
@@ -77,13 +80,12 @@ class xlsx(object):
             self.fileName = fileName
             self.wb = load_workbook(fileName, read_only=True, data_only=True)
 
-    def get_range(self, ws=None, r=None, named_range=None,
-                              header=True, transpose=False, columns = None,
-                              index=None, lerp=False):
+    def get_range(self, ws=None, r=None, named_range=None, header=True,
+                  transpose=False, columns=None, index=None, lerp=False):
         """Fetch data from a xlsx file.
 
         Parameters
-        --------------
+        ----------
 
             ws : str
                 worksheet name where to locate the range r, to also be
@@ -102,10 +104,8 @@ class xlsx(object):
             index : int
                 Column index in the original data range
 
-        Returns:
-        --------------
-
-
+        Returns
+        -------
         """
         try:
             # Si named_range est donné en arguement, on récupère
@@ -113,13 +113,15 @@ class xlsx(object):
             # get_named_range
             if named_range is not None:
                 _rn = self.wb.get_named_range(named_range).value
-                ws, r = [ _elt.replace("'","") for _elt in _rn.split('!')]
+                ws, r = [_elt.replace("'", "") for _elt in _rn.split('!')]
 
             ws = self.wb[ws]
             if transpose is False:
-                res = pd.DataFrame([[ws[_c].value for _c in _r] for _r in rows_from_range(r)])
+                res = pd.DataFrame([[ws[_c].value for _c in _r]
+                                   for _r in rows_from_range(r)])
             else:
-                res = pd.DataFrame([[ws[_c].value for _c in _r] for _r in rows_from_range(r)]).T
+                res = pd.DataFrame([[ws[_c].value for _c in _r]
+                                   for _r in rows_from_range(r)]).T
 
             if columns is not None:
                 header = False
@@ -131,7 +133,7 @@ class xlsx(object):
                 res.columns = columns
 
             if type(index) is int:
-                _index = res.iloc[:,index]
+                _index = res.iloc[:, index]
                 res.drop(res.columns[index], axis=1, inplace=True)
                 res.index = _index
 
@@ -139,7 +141,7 @@ class xlsx(object):
                 if min(res.shape) == 1:
                     if res.shape[0] < res.shape[1]:
                         res = res.T
-                    mymesh = mesh2d(res.index, res.iloc[:,0])
+                    mymesh = mesh2d(res.index, res.iloc[:, 0])
                 else:
                     mymesh = mesh3d()
                     mymesh.from_pandas(res)
@@ -155,9 +157,11 @@ class xlsx(object):
         res = mesh3d()
         return res.from_pandas(self)
 
-def quoted_form_of(s : str):
+
+def quoted_form_of(s: str):
     """Return a quoted form of a string."""
     return f"\"{s}\""
+
 
 def save_style(styleName):
     """Save matplotlib configuration into style.
@@ -171,18 +175,22 @@ def save_style(styleName):
     from cycler import Cycler
     import numbers
 
-    stylelibDir = '\\'.join([os.path.normpath(matplotlib.get_configdir()), "stylelib"])
+    stylelibDir = '\\'.join([os.path.normpath(matplotlib.get_configdir()),
+                            "stylelib"])
 
     # Check if stylelib path exists
     if not os.access(stylelibDir, os.R_OK):
         os.mkdir(stylelibDir)
 
-    with codecs.open('\\'.join([stylelibDir, os.path.basename(os.path.splitext(styleName)[0]) + ".mplstyle "]),
-                     "w", "utf-8") as f:
+    with codecs.open('\\'.join([stylelibDir,
+                               os.path.basename(os.path.splitext(styleName)[0])
+                                + ".mplstyle "]), "w", "utf-8") as f:
 
         for k in sorted(plt.rcParams):
-            if k.split(".")[0] not in ["animation", "backend", "datapath", "docstring","examples", "keymap",
-                                       "ps", "svg", "tk", "timezone", "toolbar", "verbose", "webagg"]:
+            if k.split(".")[0] not in ["animation", "backend", "datapath",
+                                       "docstring", "examples", "keymap",
+                                       "ps", "svg", "tk", "timezone",
+                                       "toolbar", "verbose", "webagg"]:
                 v = matplotlib.rcParams[k]
 
                 if v is None:
@@ -193,26 +201,27 @@ def save_style(styleName):
                     v_rc = "{}".format(v)
                     print(v_rc)
                 elif isinstance(v, Cycler):
-                    v_rc = "cycler('color', [{}])".format(",".join("'{}'".format(str(_v).replace('#',''))
-                                                                   for _v in v.by_key()['color']))
+                    v_rc = "cycler('color', [{}])"\
+                                    .format(",".join("'{}'"
+                                            .format(str(_v).replace('#', ''))
+                                            for _v in v.by_key()['color']))
                 else:
                     if isinstance(v, str) and v.startswith("#"):
-                        v_rc = v.replace('#','')
+                        v_rc = v.replace('#', '')
                     else:
                         v_rc = v
                 try:
-                    plt.rcParams[k] =  v_rc
-                    f.write("{} : {}\n".format(k,v_rc))
+                    plt.rcParams[k] = v_rc
+                    f.write(f"{f} : {v_rc}\n")
                 except:
                     try:
                         if k == "savefig.bbox":
-                            plt.rcParams[k] =  'tight'
-                        if k == "savefig.bbox":
-                            plt.rcParams[k] =  'tight'
+                            plt.rcParams[k] = 'tight'
                         else:
                             raise
                     except:
-                        print(f"failed on {k} : {v_rc}")
+                        logger.warning(f"failed on {k} : {v_rc}")
+
 
 def pdfcrop(fileName):
     """Crop a pdf file inplace.
@@ -231,7 +240,8 @@ def pdfcrop(fileName):
 
     subprocess.run(["pdfcrop", normpath(fileName), normpath(fileName)])
 
-####### PATH AND FILE RELEATED UTILIES #######
+
+#  ###### PATH AND FILE RELEATED UTILIES #######
 def get_file_list(path=None, extension=None):
     """Get the list of file in the order liked by path."""
     import fnmatch
@@ -239,15 +249,15 @@ def get_file_list(path=None, extension=None):
     from os.path import basename
 
     fileList = []
-    if isinstance(path, str) or isinstance(path, unicode) :
-        path = [ path ]
+    if isinstance(path, str) or isinstance(path, unicode):
+        path = [path]
 
-    if "*" in extension :
+    if "*" in extension:
         extension = extension
-    else :
+    else:
         extension = '*.{ext}'.format(ext=extension)
 
-    for p in path :
+    for p in path:
         if p is not None and os.path.exists(p):
             for root, dirnames, filenames in os.walk(p):
                 for f in fnmatch.filter(filenames, extension):
@@ -255,6 +265,7 @@ def get_file_list(path=None, extension=None):
                         fileList.append(os.path.join(root, f))
 
     return fileList
+
 
 def path_name_ext(path):
     """Return a tuple containing root, name ext."""
