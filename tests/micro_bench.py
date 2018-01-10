4cea5fac6d3dfe78743a2cf80b78969f808f9f6e
gwin-zegal <gwin-zegal@users.noreply.github.com>
2018-01-10 21:03:23 +0100

    Separate info test

---
+++ tests/micro_bench.py
@@ -0,0 +1,43 @@
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+from numpy import (random, array, arange, linspace, interp)
+from lerp.mesh import Mesh
+from time import time
+import numpy as np
+import pandas as pd
+
+def tiny_bench():
+
+    x = np.linspace(0, 2 * np.pi, 10)
+    y = np.sin(x)
+
+    m2d = Mesh(x,y)
+    x = m2d.x.data
+    y = m2d.data
+
+    results = {}
+    _range = np.arange(0,1_050_000, 50_000)
+
+    for N in _range:
+        _xi = np.random.randint(1, 10000, N).astype(np.float64) + np.random.random(N)
+        _xi.sort()
+        t1 = time()
+        m2d.interpolation(_xi, interp='linear', extrap='hold')
+        t2 = time()
+        interp(_xi, x, y)
+        t3 = time()
+        results[N] = [t1, t2, t3]
+
+    all_runs = pd.DataFrame(results) * 1000
+    all_runs = all_runs.T.diff(axis=1).loc[:,1:]
+    all_runs.columns = ["Mesh", "Numpy"]
+    all_runs.index.name = "Interpolated array size"
+
+    return all_runs
+
+print("*"*80)
+print("Tiny bench")
+print("*"*80)
+print(tiny_bench())
---
+++ tests/pressure.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from lerp import Mesh
+
+class PyDataSet(object):
+
+    """
+    Retrieves R Dataset
+    """
+    db = pd.read_csv("http://vincentarelbundock.github.com/Rdatasets/datasets.csv")
+    db.set_index('Item', drop=True, inplace=True)
+    
+    @classmethod
+    def search(cls, name):
+        """
+        Search a dataset """
+        #if name.isalpha():
+        #    name = name.lower()
+        return cls.db[cls.db.Title.str.contains(name, case=False) | 
+                      cls.db.index.str.contains(name, case=False)]
+    
+    @classmethod
+    def get(cls, name):
+        assert name in cls.db.Title, f"No dataset named {name}"
+        df = pd.read_csv(cls.db.loc[name].csv)
+        df.set_index(df.columns[0], drop=True, inplace=True)
+        df.index.name = 'Sample'
+        return df
+        
+    @classmethod
+    def list(cls):
+        return cls.db
+
+
+pressure = PyDataSet.get('pressure')
+pressure.plot('temperature', 'pressure')
+pm = Mesh(*pressure.values.T, label="Pression")
+
+# print(pm)
