330f0af5a7aa32ee46c86daddad766b3e5b4afb5
gwin-zegal <gwin-zegal@users.noreply.github.com>
2018-03-13 21:03:34 +0100

    Clean header

--- lerp/C/src/NDTable.c
+++ lerp/C/src/NDTable.c
@@ -35,9 +35,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 #include <stdio.h>
 #include <float.h>
-// #include <x86intrin.h>
-
-// emmintrin.h
 
 #include "NDTable.h"
 
